c89af9d8f692a4442f2922e0006bb3ae44a07aaf
Victor Corrêa <victor.correapereira@gmail.com>
2021-08-23 12:50:56 -0300

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Renders game title' , () => {
+  render(<App />);
+  const linkElement = screen.getByText("Humano do Amanhã");
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/services/httpclient.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+class HttpClient {
+
+  private configHeaders = {
+    'Content-Type': 'application/json'
+  };
+
+  constructor(){
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+    axios.defaults.headers = this.configHeaders;
+  }
+
+}
+
+export default new HttpClient();
+
+
