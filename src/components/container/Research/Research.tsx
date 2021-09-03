51e7912e2b7ea5480ac3bb44168881d8a00bd03d
Victor Corrêa <victor.correapereira@gmail.com>
2021-09-03 08:58:19 -0300

    add research logic

--- src/components/container/Research/Research.tsx
+++ src/components/container/Research/Research.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useContext, useMemo } from 'react';
 import ResearchQuestion from 'components/view/ResearchQuestion/ResearchQuestion';
-import ResearchOptions from 'components/view/ResearchOptions/ResearchOptions';
+import ResearchOptions from 'components/container/ResearchOptions/ResearchOptions';
 
 import SettingsContext from 'context/settingsContext';
 import Menu from '../Menu/Menu';
@@ -16,12 +16,88 @@ const Research: React.FC = () => {
   const { step, setStep } = useContext(SettingsContext);
   const [questions, setQuestions] = useState<ProfileQuestion[]>([]);
   const [currentQuestionIndex, setCurrentQuestionsIndex] = useState<number>(0);
+  const [gender,setGender] = useState<string>('');
+  const [otherGender, setOtherGender] = useState<string>('');
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
 
   useEffect(() => {
     Profile.getQuestion().then(res => {
-      console.log(res);
-      setQuestions(res.questions);
-    })
+      const currentQuestions = res.questions
+        .filter((q: any) => q.api_field !== 'outro_genero')
+        .map((q: any) => {
+          if(q.api_field === 'genero'){
+            return {
+              ...q,
+              options: [
+                ...q.options.filter((o: any) => o.value !== 'outro'),
+                {
+                  id: q.options.find((o: any) => o.value === 'outro').id,
+                  value: "outro",
+                  text: "Outro",
+                  optionType: 'input',
+                }
+              ],
+              answer: '',
+              answerText: '',
+            }
+          } 
+          if(q.api_field === 'onde_mora'){
+            return {
+              ...q,
+              searchable: true,
+              answer: '',
+
+            }
+          }
+          if(q.api_field === 'avaliacao_jogo'){
+            return {
+              ...q,
+              options: q.options.map((o: any) => ({...o, stars: true})),
+              answer: '',
+              answerText: '',
+            }
+          }
+          if(q.api_field === 'avaliacao_comentario'||
+            q.api_field === 'personalizado_text' ||
+            q.api_field === 'reflexao_texto'){
+            return {
+              ...q,
+              options: [
+                {
+                  value: "",
+                  text: "Responda aqui",
+                  optionType: 'input',
+                }
+              ],
+              answer: '',
+              answerText: '',
+            }
+          }
+          if(q.api_field === 'melhoria'){
+            return {
+              ...q,
+              options: [
+                ...q.options.filter((o: any) => o.value !== 'outro' && o.value !== 'OTHER'),
+                {
+                  id: q.options?.find((o: any) => o.value === 'outro' || o.value === 'OTHER').id || '',
+                  value: "OTHER",
+                  text: "Outro",
+                  optionType: 'input',
+                }
+              ],
+              answer: '',
+              answerText: '',
+            }
+          }
+          return {
+            ...q,
+            answer: '',
+            answerText: '',
+          }
+        });
+      setQuestions(currentQuestions);
+    });
   }, []);
 
   const goToQuiz = useCallback(() => {
@@ -49,16 +125,29 @@ const Research: React.FC = () => {
     if(nextIndex < questions.length){
       setCurrentQuestionsIndex(nextIndex);
     } else {
+
       goToResult();
     }
   }, [currentQuestionIndex, questions]);
 
-  const setAnswer = useCallback((value) => {
+  const setAnswer = useCallback((value, textValue?) => {
     const updatedQuestions = [...questions];
     updatedQuestions[currentQuestionIndex].answer = value;
+    if(textValue){
+      updatedQuestions[currentQuestionIndex].answerText = textValue;
+    }
+
     setQuestions(updatedQuestions);
     goToNextQuestion();
   }, [currentQuestion, questions, goToNextQuestion]);
+
+  const questionOptions = useMemo(() => {
+    if(currentQuestion){
+      if(currentQuestion.searchable){
+        return currentQuestion.options.filter(opt => opt.text.toLowerCase().includes(searchQuery.toLowerCase()));
+      } return currentQuestion.options;
+    } return [];
+  }, [searchQuery, currentQuestion]);
 
   return (
     <>
@@ -67,11 +156,17 @@ const Research: React.FC = () => {
         <Menu text="Texto" prevStep={'quiz'} prevAction={currentQuestionIndex ? goToPreviousQuestion : null} />
         {!!questions.length ? (
           <>
-            <ResearchQuestion question={currentQuestion.text} />
+            <ResearchQuestion question={currentQuestion} />
             <div className={styles.sidebar}>
-              <ResearchOptions options={currentQuestion.options} onSelect={setAnswer} />
+              {!!currentQuestion.searchable && 
+                <input
+                  value={searchQuery}
+                  onChange={e => setSearchQuery(e.currentTarget.value)}
+                  placeholder="Escreva aqui"
+                />
+              }
+              <ResearchOptions options={questionOptions} onSelect={setAnswer} />
               <div>
-                <button onClick={goToResult} style={{ display: 'block', marginBottom: '16px'}}>Próxima fase</button>
               </div>
             </div>
           </>
--- src/components/view/ResearchQuestion/ResearchQuestion.tsx
+++ src/components/view/ResearchQuestion/ResearchQuestion.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useMemo, useContext } from 'react';
+
+import SettingsContext from 'context/settingsContext';
 
 import globalStyles from 'globals.module.scss';
+import { ProfileQuestion } from 'types/types';
+
+import Video from '../Video/Video';
 
 type ResearchQuestionProps = {
-  question: string;
+  question: ProfileQuestion;
 }
 
 
 const ResearchQuestion: React.FC<ResearchQuestionProps> = ({ question }) => {
+
+  const { libras } = useContext(SettingsContext);
+
+  const content = useMemo(() => {
+    if(question){
+      if(libras && question?.texto_libras?.url){
+        return <Video source={question?.texto_libras?.url || 'http://techslides.com/demos/sample-videos/small.mp4'}/>;
+      } return <h1>{question.text}</h1>
+    } return null;
+  },[libras, question]);
+
+
   return (
     <div className={globalStyles.content}>
-      <h1>{question}</h1>
+      {content}
     </div>
   );
 }
